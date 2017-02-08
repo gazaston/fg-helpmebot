6def11c3eec6aff4aa7e5413ad9908ce80c1ddef
Gaz Aston <spiritbased@gmail.com>
2017-02-08 17:50:24 +0000

    text change

--- app.rb
+++ app.rb
@@ -9,5 +9,5 @@ end
  
 def postback channel
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => "https://docs.google.com/a/wearefuturegov.com/spreadsheets/d/1Hc_ktX_3XMAAbli0ZlV0VSI5pxxAEYJkWf4I_Zjc2vQ/edit?usp=sharing", "username" => "Freebot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+    HTTParty.post slack_webhook, body: {"text" => "Update your free time!\nhttps://docs.google.com/a/wearefuturegov.com/spreadsheets/d/1Hc_ktX_3XMAAbli0ZlV0VSI5pxxAEYJkWf4I_Zjc2vQ/edit?usp=sharing", "username" => "Freebot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
 end
