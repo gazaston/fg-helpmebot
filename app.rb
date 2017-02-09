d7cd45cc397871fa30f800c2b0b4ccdaf081c0c4
Gaz Aston <spiritbased@gmail.com>
2017-02-09 14:14:48 +0000

    change params and endpoint

--- app.rb
+++ app.rb
@@ -2,12 +2,22 @@ require 'sinatra'
 require 'httparty'
 require 'json'
  
-post '/imfree' do
+post '/helpme' do
   postback params[:channel_id]
   status 200
 end
  
-def postback channel
+def postback(channel, user)
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
-    HTTParty.post slack_webhook, body: {"text" => "Update your free time!\nhttps://docs.google.com/a/wearefuturegov.com/spreadsheets/d/1Hc_ktX_3XMAAbli0ZlV0VSI5pxxAEYJkWf4I_Zjc2vQ/edit?usp=sharing", "username" => "Freebot", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
+    
+    # if discipline = ""
+
+
+    HTTParty.post(slack_webhook, body: {
+        "text" => "hey #{user}", 
+        "username" => "Freebot", 
+        "channel" => params[:channel_id]}.to_json, 
+        headers: {'content-type' => 'application/json'
+      }
+    )
 end
