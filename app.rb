a415a3b1a89d3e9afb593a3d1f1e3d4ad6aa2b47
Gaz Aston <spiritbased@gmail.com>
2017-02-09 15:04:16 +0000

    add categories

--- app.rb
+++ app.rb
@@ -8,13 +8,39 @@ post '/helpme' do
 end
  
 def postback(channel, user_id, user_name, text)
-    slack_webhook = ENV['SLACK_WEBHOOK_URL']    
+    slack_webhook = ENV['SLACK_WEBHOOK_URL']   
+
+  text = text.downcase 
+
+  # graphic, service, strategy, organisation/business, product, development, tech, change, project mgmt, audio/video, finance, admin, data
 
   case text
     when "graphic"
       response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
     when "service"
       response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "strategy"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "organisation"
+      response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "product"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "development"
+      response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "tech"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "change"
+      response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "project mgmt"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "audio/video"
+      response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "finance"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "admin"
+      response ||= "Hey. <@#{user_name}> needs some help with some service design. Can you help, <@gaz>, <@willr>, <@ale>?"
+    when "data"
+      response ||= "Hey. <@#{user_name}> needs some help with some graphic design. Can you help, <@gaz>, <@willr>, <@ale>?"
     else
       response ||= "Sorry, I didn't get that…"
   end
