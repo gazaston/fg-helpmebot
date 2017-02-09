5fd1aee9a5de4c4dcee44a93d7aa31929e24134f
Gaz Aston <spiritbased@gmail.com>
2017-02-09 14:32:42 +0000

    pass text as param

--- app.rb
+++ app.rb
@@ -3,18 +3,18 @@ require 'httparty'
 require 'json'
  
 post '/helpme' do
-  postback(params[:channel_id], params[:user_id], params[:user_name])
+  postback(params[:channel_id], params[:user_id], params[:user_name], params[:text])
   status 200
 end
  
-def postback(channel, user_id, user_name)
+def postback(channel, user_id, user_name, text)
     slack_webhook = ENV['SLACK_WEBHOOK_URL']
     
     # if discipline = ""
 
 
     HTTParty.post(slack_webhook, body: {
-        "text" => "hey <@#{user_name}>", 
+        "text" => "Hey. <@#{user_name}> needs some help with #{text}", 
         "username" => user_name, 
         "channel" => params[:channel_id]}.to_json, 
         headers: {'content-type' => 'application/json'
