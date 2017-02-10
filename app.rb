707677735b77d5cd4d39aa782ceaa2b34fd83cd7
Gaz Aston <spiritbased@gmail.com>
2017-02-10 19:04:28 +0000

    hook up to api.ai

--- app.rb
+++ app.rb
@@ -7,76 +7,64 @@ Dotenv.load
 
  
 post '/helpme' do
-  # postback(params[:channel_id], params[:user_id], params[:user_name], params[:text])
-  # "hi"
-  # status 200
   # logger.info JSON.parse(request.body.read.to_s)
-  responder
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+  responder(data)
 end
 
-def responder
-  # client = ApiAiRuby::Client.new(:client_access_token => ENV['API_AI_ACCESS_KEY'])
-
-  client = ApiAiRuby::Client.new(
-    client_access_token: '1ad258b74e4a4654a7ff8f9bb086a3fc'
-  )
-
-  response = client.text_request 'hello!'
-  logger.info JSON.parse(request.body.read.to_s)
-  
+def responder(data)
+  user_name = data["originalRequest"]["data"]["user"].to_s
+  case data["result"]["parameters"]["discipline"]
+    when "graphic"
+      response ||= "Hey. <@#{user_name}> needs some help with some *graphic design.* Can you help, <@ale>, <@kj>, <@elle>, <@nina>, <@chris>, <@gaz>?"
+    when "service"
+      response ||= "Hey. <@#{user_name}> needs some help with some *service design.* Can you help, <@ale>, <@kj>, <@elle>, <@lingjing>, <@nina>, <@emilybazalgette>, <@simone>, <@jan>?"
+    when "strategy"
+      response ||= "Hey. <@#{user_name}> needs some help with some *strategy work.* Can you help, <@ale>, <@kj>, <@elle>, <@lingjing>, <@nina>, <@emilybazalgette>, <@adam>, <@simone>, <@jrae>, <@alex>, <@emmamcg>, <@benunsworth>, <@willr>, <@matt>?"
+    when "organisation"
+      response ||= "Hey. <@#{user_name}> needs some help with some *org design.* Can you help, <@lingjing>, <@nina>, <@emilybazalgette>, <@adam>, <@simone>, <@emmamcg>, <@benunsworth>?"
+    when "product"
+      response ||= "Hey. <@#{user_name}> needs some help with some *product work.* Can you help, <@kj>, <@elle>, <@lingjing>, <@chris>, <@gaz>, <@jrae>, <@alex>, <@matt>?"
+    when "development"
+      response ||= "Hey. <@#{user_name}> needs some help with some *development work.* Can you help, <@chris>, <@gaz>, <@jrae>, <@alex>?"
+    when "tech"
+      response ||= "Hey. <@#{user_name}> needs some help with some *tech work.* Can you help, <@chris>, <@gaz>, <@jrae>, <@alex>, <@benunsworth>, <@willr>, <@matt>?"
+    when "change"
+      response ||= "Hey. <@#{user_name}> needs some help with some *change work.* Can you help, <@nina>, <@emilybazalgette>, <@adam>, <@alex>, <@benunsworth>?"
+    when "project mgmt"
+      response ||= "Hey. <@#{user_name}> needs some help with some *project management work.* Can you help, <@nina>, <@emilybazalgette>, <@adam>, <@alex>, <@benunsworth>?"
+    when "audio/video"
+      response ||= "Hey. <@#{user_name}> needs some help with some *audio/video work.* Can you help, <@elle>, <@chris>, <@gaz>?"
+    when "finance"
+      response ||= "Hey. <@#{user_name}> needs some help with some *finance work.* Can you help, <@roberta>, <@matt>?"
+    when "admin"
+      response ||= "Hey. <@#{user_name}> needs some help with some *admin.* Can you help, <@roberta>, <@oana>?"
+    when "data"
+      response ||= "Hey. <@#{user_name}> needs some help with some *data work.* Can you help, <@nina>, <@adam>, <@gaz>, <@benunsworth>?"
+    when "writing"
+      response ||= "Hey. <@#{user_name}> needs some help with some *writing/editing work.* Can you help, <@nina>, <@adam>, <@gaz>, <@benunsworth>?"
+    else
+      response ||= "Sorry, but that isn't in the list of categories I have. I'm not too smart so you'll need to enter a category from the list in the message pinned to this channel 😘"
+  end
+  content_type :json
+  {
+    "speech" => response,
+    "displayText" => response,
+    "data" => {
+      "slack" => {
+        "text" => response,
+        "attachments" => [
+          {
+            # "text" => "And here's an attachment!!",
+            "fallback" => "Help!",
+            "color" => "#1072ae",
+            "footer" => "Won't you please, please help me?",
+            "footer_icon" => "https://d3uepj124s5rcx.cloudfront.net/items/403t421f0U1d0B2R111c/Lennon.jpg"
+          }
+        ]
+      }
+    },
+    headers: {'content-type' => 'application/json'}
+  }.to_json
 end
-
-
-
-# def api_ai_client
-#   client = ApiAiRuby::Client.new(:client_access_token => ENV['1ad258b74e4a4654a7ff8f9bb086a3fc'])
-# end
- 
-# def postback(channel, user_id, user_name, text)
-#   # client = ApiAiRuby::Client.new(:client_access_token => ENV['1ad258b74e4a4654a7ff8f9bb086a3fc'])
-#   slack_webhook = ENV['SLACK_WEBHOOK_URL']   
-
-#   text = text.downcase 
-
-#   # graphic, service, strategy, organisation/business, product, development, tech, change, project mgmt, audio/video, finance, admin, data
-
-#   case text
-#     when "graphic"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *graphic design.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "service"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *service design.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "strategy"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *strategy work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "organisation"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *org design.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "product"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *product work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "development"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *development work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "tech"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *tech work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "change"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *change work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "project mgmt"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *project management work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "audio/video"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *audio/video work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "finance"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *finance work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "admin"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *admin.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     when "data"
-#       response ||= "Hey. <@#{user_name}> needs some help with some *data work.* Can you help, <@gaz>, <@willr>, <@ale>?"
-#     else
-#       response ||= "Sorry, but *#{text}* isn't in the list of categories I have. I'm not too smart so you'll need to enter a category from the list in the message pinned to this channel 😘"
-#   end
-
-
-#     HTTParty.post(slack_webhook, body: {
-#         "text" => response, 
-#         "username" => "Help Me Bot", 
-#         "channel" => params[:channel_id]}.to_json, 
-#         headers: {'content-type' => 'application/json'
-#       }
-#     )
-# end
